ea00bdd51b47a40078ab4a622fb367c9661f8f22
MrCyclopede <romain.courtois.mails@gmail.com>
2022-05-29 23:05:05 +0200

    chat ok

--- chat.py
+++ chat.py
@@ -5,38 +5,72 @@ from os import environ
 import txaio
 txaio.use_asyncio()
 from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
+from autobahn.wamp import ApplicationError
 import pytchat
 import pickle
+import json
 
 from input import sanitize_very_strict
 
 
+
         
 class Chat(ApplicationSession):
-    
-
-    def spawn_from_message(message):
-            
-            pass
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.call = self.custom_call
         
-    async def board_changed(self):
-            self.board = pickle.loads(await self.call("board.get"))
-            print(self.board)
-            print(type(self.board))
+    async def custom_call(self, *args):
+        super().call(*args)
+        # try:
+        #     super().call(*args)
+        # except ApplicationError as e:
+        #     print(e)
             
+    async def spawn_from_message(self, message):
+        # id = await self.call('ensure_player_exists', )
+        player_data ={
+                    "display_name": sanitize_very_strict(message['author']['name']),
+                    "username": message['author']['name'],
+                    "alive": 1,
+                    "current_alive":0,
+                    # "best_alive": "INTEGER",
+                    "last_checked": message['timestamp'],
+                    "channel_id": message['author']['channelId'],
+                }
+        try:
+            id = await self.call("data.create_player", player_data)
+        except Exception as e:
+            print(f"    (ERROR): data.create_player failed for {player_data['username']}")
+        try:
+            
+            await self.call("spawn.slime.random", id, player_data)
+        except Exception as e:
+            print(f"     (ERROR): spawn.slime.random failed with {e}")
+        return id
             
     async def onJoin(self, details):
-        # resp = await self.call("board.get")
-        # self.board = pickle.loads(resp)
+        self.stream_id = "5qap5aO4i9A"
+        self.stream_url = f"https://www.youtube.com/watch?v={self.stream_id}"
         
-                
-        stream_id = "5qap5aO4i9A"
-        stream_url = f"https://www.youtube.com/watch?v={stream_id}"
-        chat = pytchat.create(video_id=stream_url,interruptable=False, hold_exception=False)
-                    
+        chat = pytchat.create(video_id=self.stream_url,interruptable=False, hold_exception=False)
+        
+        
+        
         while chat.is_alive():
             for c in chat.get().sync_items():
-                message = c.json()
+                message = json.loads(c.json())
+                
+                print("=====MESSAGE================")
+                name = message['author']['name']
+                text = "".join(s for s in message['messageEx'] if isinstance(s, str))
+                print(f"[{name}]: {text}")
+                print("============================")
+                
+                id = await self.call('data.player.exist', message['author']['channelId'])
+                
+                await self.spawn_from_message(message)
+                
         
 
 if __name__ == '__main__':
