5772b46e9e5f267cdb55ea4d509decd6b159a8e4
MrCyclopede <romain.courtois.mails@gmail.com>
2022-05-31 06:37:31 +0200

    clean

---
+++ template_components/pubsub_sender.py
@@ -0,0 +1,22 @@
+import asyncio
+from os import environ
+import txaio
+txaio.use_asyncio()
+from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
+
+class Component(ApplicationSession):
+    """
+    An application component that publishes an event every second.
+    """
+
+    async def onJoin(self, details):
+        counter = 0
+        while True:
+            print("publish: com.myapp.topic1", counter)
+            self.publish('com.myapp.topic1', counter)
+            counter += 1
+            await asyncio.sleep(1)
+        
+if __name__ == '__main__':
+    runner = ApplicationRunner("ws://127.0.0.1:8080/ws", "realm1")
+    runner.run(Component)
