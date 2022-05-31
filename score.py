449d14a1ed610a20a7698a55835c1544697b14d7
MrCyclopede <romain.courtois.mails@gmail.com>
2022-05-31 15:02:13 +0200

    working x2

--- score.py
+++ score.py
@@ -9,6 +9,7 @@ from time import time
 from math import floor
 import pprint
 import pickle
+import random
 
 
 SCOREBOARD_WIDTH = 10
@@ -26,24 +27,15 @@ class Component(ApplicationSession):
                     f"bossbar set minecraft:peglin name \"High score: {self.highscore}\""
             )
         self.names = await self.call('gamestate.names.all')
+        self.gamestate = await self.call('gamestate.get')
         self.scores = self.reset_scores()
         
         await self.check_alive()
         
-
+        self.register(self.reset_scores,'score.reset')
         self.subscribe(self.check_alive, "game.tick")
 
-    def get_ypos(self, haystack):
-        hs = haystack
-
-        pos_start = hs.find("Pos: ")
-        hs = hs[pos_start:]
-        comma_left = hs.find(",")
-        hs = hs[comma_left + 2 :]
-        comma_right = hs.find(",")
-        hs = hs[: comma_right - 1]
-
-        return int(float(hs))
+    
     
     async def kill(self, uid, name):
         cmd = f"kill @e[tag={uid}, type=!player]"
@@ -57,7 +49,7 @@ class Component(ApplicationSession):
             # "score": 
         }
         
-        self.delete_score(uid, name)
+        self.delete_player(uid, name)
         await self.call("gamestate.names.remove", uid)
         
         if not str(uid) in self.names:
@@ -76,10 +68,33 @@ class Component(ApplicationSession):
         await self.call("data.player.update", uid, updated_data)
         
     
+    def get_pos(self, haystack):
+        hs = haystack
 
+        pos_start = hs.find("Pos: ")
+        
+        hs = str(hs[pos_start:])
+        hsold = hs
+        hs = hs[hs.find("[")+1:hs.find("]")]
+        hs = hs.split(', ')
+        
+        ret = (int(float(hs[0][:-1])), int(float(hs[1][:-1])), int(float(hs[2][:-1])))
+        return (ret)
+        
+    
     async def check_alive(self):
+        
+        # await self.call('minecraft.post', f"execute as @e[tag=ball,x={self.gamestate['origin_x']},y={self.gamestate['origin_y']},z={self.gamestate['origin_z']},dx={self.gamestate['depth']},dz={int(self.gamestate['width'] / 3)},dy=-10] at @s run tp @s ~ ~{self.gamestate['height'] - 3} ~")
+        # await self.call('minecraft.post', f"execute as @e[tag=ball,x={self.gamestate['origin_x']},y={self.gamestate['origin_y']},z={self.gamestate['origin_z']},dx={self.gamestate['depth']},dz={int(self.gamestate['width'] / 3)},dy=-10] at @s run tp @s ~ ~{self.gamestate['height'] - 3} ~")
+        # await self.call('minecraft.post', f"execute as @e[tag=ball,x={self.gamestate['origin_x']},y={self.gamestate['origin_y']},z={self.gamestate['origin_z']},dx={self.gamestate['depth']},dz={int(self.gamestate['width'] / 3)},dy=-10] at @s run tp @s ~ ~{self.gamestate['height'] - 3} ~")
+        
         self.names = await self.call('gamestate.names.all')
         print(f"\no--> Check alive on {len(self.names)} players")
+        
+        
+        
+        
+        
         
         
         names_copy = self.names.copy()
@@ -89,16 +104,33 @@ class Component(ApplicationSession):
             cmd = f"data get entity @e[tag={str(i)}, tag=ball, limit=1]"
             ret = await self.call("minecraft.post", cmd)
             if "entity data" in ret:
-                y_pos = self.get_ypos(ret)
+                x_pos, y_pos, z_pos = self.get_pos(ret)
             else:
-                y_pos = None
+                x_pos, y_pos, z_pos = (None,  None, None)
 
-            if not isinstance(y_pos, int) or y_pos <= 0:
-                #SHOULD BE KILLED
-                
+            # print(x_pos, y_pos, z_pos)
+            
+            
+            if not isinstance(y_pos, int):
                 await self.kill(i, names_copy[str(i)])
             else:
-                #STAY ALIVE
+                if y_pos <= 5:
+                    if z_pos - self.gamestate['origin_z'] <= int(self.gamestate['width'] / 5):
+                        print(f"respawning le pote: {i} {names_copy[str(i)]}, z= {z_pos}")
+                        self.call('minecraft.post', f"execute as @e[tag=ball, tag={i}] at @s run tp {(self.gamestate['origin_x'] + self.gamestate['depth']) / 2} ~{self.gamestate['height'] + self.gamestate['origin_y'] - 2} {random.randrange(self.gamestate['origin_z'], self.gamestate['width'])}")
+                    # elif x_pos <= self.gamestate['origin_x'] <= 2 * int(self.gamestate['width'] / 3):
+                        # print("kill le pote: {i} {names_copy[str(i)]}")
+                        # await self.kill(i, names_copy[str(i)])
+                    else:
+                        print(f"x2 le pote le pote: {i} {names_copy[str(i)]}")
+                        await self.kill(i, names_copy[str(i)])
+                        pass
+                        
+                        
+                #respawn bucket
+                
+                
+                
                 if not str(i) in self.scores.keys():
                     self.scores[str(i)] = 1
                 else: 
@@ -118,10 +150,16 @@ class Component(ApplicationSession):
             tag = entry[0]
             # print(f"{tag=}")
             if tag not in names:
-                return print(f"[[[[[[name missing on {tag} ]]]]]]]")
-            name = names[tag]
+                data = pickle.loads(await self.call('data.player.read', tag))
+                name = data['display_name']
+                # self.delete_player(tag, name)
+                print(f"[[[[[[name was on {tag} , it's now: {name}]]]]]]]")
+                return
+                
+            else:
+                name = names[tag]
 
-            if not "PortalHub" in name:
+            if not "PortalHub" in name and not "Nightbot" in name:
                 ret = await self.call('minecraft.post', 
                     f"scoreboard players set {name[:SCOREBOARD_WIDTH]} alive_for {self.scores[tag]}"
                 )
@@ -139,11 +177,9 @@ class Component(ApplicationSession):
         )
         self.call('minecraft.post', cmd)
 
-    # def next_round(derp):
-    #     cmd = f"execute as @e[tag=[{i}, ball]] at @s run tp @s ~ {self.gamestate.origin_y + self.gamestate.height + 2} ~"
-    #         self.call('minecraft.post', cmd)
-    def delete_score(self, uid, name):
-        self.call('minecraft.post', f"scoreboard players set {name[:SCOREBOARD_WIDTH]} alive_for 0")
+
+    def delete_player(self, uid, name):
+        # self.call('minecraft.post', f"scoreboard players set {name[:SCOREBOARD_WIDTH]} alive_for 0")
         self.call('minecraft.post', f"scoreboard players reset {name[:SCOREBOARD_WIDTH]} alive_for")
         
         if str(uid) not in self.scores:
@@ -160,10 +196,11 @@ class Component(ApplicationSession):
         self.call('minecraft.post', 
             f"title funyrom actionbar \"{name} died after {self.scores[str(uid)]} seconds\""
         )
+        # player_update_id[]
 
     def onDisconnect(self):
         for uid in self.scores:
-            self.delete_score(uid, self.names[uid])
+            self.delete_player(uid, self.names[uid])
         asyncio.get_event_loop().stop()
 
 
