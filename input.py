b261484911badb53a12c2af8b310510825817139
MrCyclopede <romain.courtois.mails@gmail.com>
2022-05-31 10:39:02 +0200

    input less strict

--- input.py
+++ input.py
@@ -7,3 +7,12 @@ def sanitize_very_strict(str):
         for c in str
         if c in "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
     )
+    
+def sanitize_less_strict(str):
+    if "NAKED" in str:
+        return "notnakedbot"
+    return "".join(
+        c
+        for c in str
+        if c in " []ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"
+    )
